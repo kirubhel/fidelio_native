b26978f6710d6bebbe39f39c056ea54bb45d8b29
kirubhel <kirub.hel@gmail.com>
2026-08-13 10:46:24 +0300

    chore: update build dependencies, upgrade target SDK to 35, and increment version to 1.1.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "world.respect.kokebfidel"
         minSdk = 24
         targetSdk = 35
-        versionCode = 9
-        versionName = "1.1.6"
+        versionCode = 11
+        versionName = "1.1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
